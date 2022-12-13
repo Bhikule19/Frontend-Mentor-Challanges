f63002f5b5ee3161bfaa7e80bbdd9a48d8f4f777
Abhishek <abhishekbhikule76@gmail.com>
2022-12-13 18:56:23 +0530

    "Added another project"

---
+++ base-apparel-coming-soon-master/script.js
@@ -0,0 +1,22 @@
+const form = document.getElementById("form");
+const email = document.getElementById("email");
+
+form.addEventListener("click", (e) => {
+  e.preventDefault();
+  const emailVal = email.value;
+  console.log(emailVal);
+
+  // check if it is a valid email
+  if (!validateEmail(emailVal)) {
+    form.classList.add("error");
+  } else {
+    form.classList.remove("error");
+    document.body.innerHTML = `<div style="display:flex; align-items: center; justify-content: center;"><h1>Thank you!</h1></div>`;
+  }
+});
+
+function validateEmail(email) {
+  var re =
+    /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+  return re.test(String(email).toLowerCase());
+}
